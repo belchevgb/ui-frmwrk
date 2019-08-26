601df0f620c8a947bf15da5eb722d28daa41cb17
belchevgb <gabriel.belchev@protonmail.com>
2019-08-26 09:03:19 +0300

    Fix test issues.

--- config/tests/karma-production.config.js
+++ config/tests/karma-production.config.js
@@ -3,6 +3,7 @@ const config = require("./karma-main.config");
 config.webpack.mode = "production";
 config.concurrency = Infinity;
 config.browsers = ["ChromeHeadless"];
+config.singleRun = true;
 
 module.exports = function(cfg) {
     cfg.set(config);
